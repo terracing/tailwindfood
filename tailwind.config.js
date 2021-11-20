0aba618d173ab44a93c4234eed85e7602a8760fc
Omar Terrazas <terrazas.omar@gmail.com>
2021-11-19 23:22:00 -0600

    Extended colors for header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#F3C614',
+        'text': '#353535',
+        'secondary': '#6E6E6E'
+      }
+    },
   },
   variants: {
     extend: {},
